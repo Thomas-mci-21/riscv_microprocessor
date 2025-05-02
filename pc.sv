9165462adf9c78c721f5718370f7e0355d5354b1
eecsyws <134205468+eecsyws@users.noreply.github.com>
2025-05-02 17:23:10 +0800

    Update pc.sv

--- pc.sv
+++ pc.sv
@@ -1,3 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2024/03/06 20:09:18
+// Design Name: 
+// Module Name: pc
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
 module pc#(
     parameter   DATAWIDTH = 32
 )(
@@ -6,12 +28,12 @@ module pc#(
     input  logic [DATAWIDTH - 1:0] npc  ,
     output logic [DATAWIDTH - 1:0] pc_out   
 );
-    always_ff @(posedge ck or posedge rst) begin
+    always_ff @(posedge clk or posedge rst) begin  // 将 ck 改为 clk
         if(rst) begin
             pc_out <= 32'b0;
         end
         else begin
-            pc_out <= pc;
+            pc_out <= npc;  // 将 pc 改为 npc
         end
     end
 endmodule
